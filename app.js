8b9eedbad4d196663cbb551cf6e765b0f7038d46
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-02-18 10:48:50 -0500

    console log test for single show data

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault()
     const newSearch = this.elements.query.value
     const res = await axios.get(`http://api.tvmaze.com/singlesearch/shows?q=${newSearch}`)
-    console.log(res)
+    retrieveTVShow(res)
 })
+
+const retrieveTVShow = (show) => {
+    console.log(show.data.name);
+    console.log(show.data.image.medium);
+    console.log(show.data.network.name);
+    console.log(show.data.genres);
+    console.log(show.data.rating.average);
+}
