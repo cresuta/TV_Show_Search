57692c4f4d436ed2fd665ec624a2ae5dc37eae8b
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-02-23 12:00:40 -0500

    removing h3 and img element on html doc, creating those elements with js

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ form.addEventListener('submit', async function (e) {
 })
 
 const retrieveTVShow = (show) => {
-    const name = document.querySelector('h3')
-    const img = document.querySelector('img')
-    const showDetails = document.querySelector('#showDetails')
-    const networkName = document.createElement('li')
-    const genres = document.createElement('li')
-    const ratingAvg = document.createElement('li')
-    name.innerText = show.data.name
-    img.src = show.data.image.medium
-    networkName.innerText = `Network - ${show.data.network.name}`
-    genres.innerText = `Genres - ${show.data.genres}`
-    ratingAvg.innerText = `Rating - ${show.data.rating.average}/10`
+    const name = document.createElement('h3');
+    const img = document.createElement('img');
+    const showDetails = document.querySelector('#showDetails');
+    const networkName = document.createElement('li');
+    const genres = document.createElement('li');
+    const ratingAvg = document.createElement('li');
+    img.src = show.data.image.medium;
+    networkName.innerText = `Network - ${show.data.network.name}`;
+    genres.innerText = `Genres - ${show.data.genres}`;
+    ratingAvg.innerText = `Rating - ${show.data.rating.average}/10`;
+    showDetails.appendChild(img);
     showDetails.appendChild(networkName);
     showDetails.appendChild(genres);
     showDetails.appendChild(ratingAvg);
