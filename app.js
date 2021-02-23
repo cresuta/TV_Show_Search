3f07004caae2a34b136af4e39adc445c5814d8b9
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-02-23 11:53:00 -0500

    reverted back to single show search result

--- app.js
+++ app.js
@@ -2,27 +2,23 @@ const form = document.querySelector('#searchForm')
 form.addEventListener('submit', async function (e) {
     e.preventDefault()
     const newSearch = this.elements.query.value
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${newSearch}`)
-    retrieveTVShow(res.data)
+    const res = await axios.get(`http://api.tvmaze.com/singlesearch/shows?q=${newSearch}`)
+    retrieveTVShow(res)
 })
 
-const retrieveTVShow = (shows) => {
-    for (let result of shows) {
-        if (result.show.image){
-            const name = document.querySelector('h3')
-            const img = document.querySelector('img')
-            const showDetails = document.querySelector('#showDetails')
-            const networkName = document.createElement('li')
-            const genres = document.createElement('li')
-            const ratingAvg = document.createElement('li')
-            name.innerText = result.show.name
-            img.src = result.show.image.medium
-            networkName.innerText = `Network - ${result.show.network.name}`
-            genres.innerText = `Genres - ${result.show.genres}`
-            ratingAvg.innerText = `Rating - ${result.show.rating.average}/10`
-            showDetails.appendChild(networkName);
-            showDetails.appendChild(genres);
-            showDetails.appendChild(ratingAvg);
-        }
-    }
+const retrieveTVShow = (show) => {
+    const name = document.querySelector('h3')
+    const img = document.querySelector('img')
+    const showDetails = document.querySelector('#showDetails')
+    const networkName = document.createElement('li')
+    const genres = document.createElement('li')
+    const ratingAvg = document.createElement('li')
+    name.innerText = show.data.name
+    img.src = show.data.image.medium
+    networkName.innerText = `Network - ${show.data.network.name}`
+    genres.innerText = `Genres - ${show.data.genres}`
+    ratingAvg.innerText = `Rating - ${show.data.rating.average}/10`
+    showDetails.appendChild(networkName);
+    showDetails.appendChild(genres);
+    showDetails.appendChild(ratingAvg);
 }
